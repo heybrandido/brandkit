38ebf5a286180115894936e1936ece62b4c24a00
heybrandido <federico.gaitpiana@gmail.com>
2026-08-04 10:22:59 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/remove-bg',
+        destination: 'https://api.remove.bg/v1.0/removebg',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/remove-bg',
+        headers: [
+          { key: 'X-Api-Key', value: process.env.REMOVEBG_API_KEY || '' },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
